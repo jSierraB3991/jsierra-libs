7ccce6e65ea2aa887a1678d4d2a78d549c6ab9d1
Eliot Anderson <eliotandelon@gmail.com>
2024-10-09 16:05:21 -0500

    add generate random code

--- strings.go
+++ strings.go
@@ -1,7 +1,11 @@
 package eliotlibs
 
 import (
+	"crypto/rand"
+	"errors"
+	"math/big"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -49,4 +53,27 @@ func GetStringToTimeShortFormat(date time.Time) string {
 func GetStringToTimeLongFormat(date time.Time) string {
 	return GetStringNumberFor(date.Year()) + "/" + GetStringNumberFor(int(date.Month())) + "/" + GetStringNumberFor(date.Day()) + " " +
 		GetStringNumberFor(date.Hour()) + ":" + GetStringNumberFor(date.Minute()) + ":" + GetStringNumberFor(date.Second())
+}
+
+func GenerateRandomCode(length int, charSet string) (string, error) {
+	if length <= 0 {
+		return "", errors.New("length must be greater than 0")
+	}
+
+	if len(charSet) == 0 {
+		return "", errors.New("character set cannot be empty")
+	}
+
+	var result strings.Builder
+	charSetLength := big.NewInt(int64(len(charSet)))
+
+	for i := 0; i < length; i++ {
+		randomIndex, err := rand.Int(rand.Reader, charSetLength)
+		if err != nil {
+			return "", err
+		}
+		result.WriteByte(charSet[randomIndex.Int64()])
+	}
+
+	return result.String(), nil
 }
