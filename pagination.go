9b108a8754a040a6e28767fabf420260f6be0a05
Juan David Sierra <judas3991@gmail.com>
2025-02-22 19:10:50 -0500

    add true pagination in jsierra libs

--- pagination.go
+++ pagination.go
@@ -1,13 +1,45 @@
 package eliotlibs
 
-func GetFirstForPagination(limit, page int) int {
-	if page == 1 {
-		return 1
-	}
-
-	return limit + 1
+type Paggination struct {
+	Limit      int         `json:"limit"`
+	Page       int         `json:"page"`
+	TotalRows  int64       `json:"rows"`
+	TotalPages int         `json:"pages"`
+	Data       interface{} `json:"data"`
+	Sort       string      `json:"-"`
 }
 
-func GetMaxForPagination(limit, page int) int {
-	return page * limit
+func (p *Paggination) GetOffset() int {
+	return (p.GetPage() - 1) * p.GetLimit()
+}
+
+func (p *Paggination) GetLimit() int {
+	if p.Limit == 0 {
+		p.Limit = 10
+	}
+	return p.Limit
+}
+
+func (p *Paggination) GetPage() int {
+	if p.Page == 0 {
+		p.Page = 1
+	}
+	return p.Page
+}
+
+func (p *Paggination) GetSort() string {
+	if p.Sort == "" {
+		p.Sort = "Id desc"
+	}
+	return p.Sort
+}
+
+type PagginationParam struct {
+	Where string
+	Data  interface{}
+}
+
+type PreloadParams struct {
+	Preload          string
+	PagginationParam PagginationParam
 }
