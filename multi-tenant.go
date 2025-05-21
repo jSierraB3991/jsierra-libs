64e28f361580de60470e2c857211204d11cfbf45
Juan David Sierra <judas3991@gmail.com>
2025-05-21 15:34:24 -0500

    add configuration for multi-tenant

---
+++ multi-tenant.go
@@ -0,0 +1,44 @@
+package eliotlibs
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type contextKey string
+
+const (
+	ContextTenantKey contextKey = "tenant_id"
+)
+
+func FromContext(ctx context.Context) (string, bool) {
+	v, ok := ctx.Value(ContextTenantKey).(string)
+	return v, ok
+}
+
+func QuoteIdentifier(identifier string) string {
+	return `"` + strings.ReplaceAll(identifier, `"`, `""`) + `"`
+}
+
+func WithTenant(ctx context.Context) (*string, error) {
+	tenant, isOk := FromContext(ctx)
+	if !isOk {
+		return nil, fmt.Errorf("tenant ID not found in context")
+	}
+	// Validar de tenant ID
+	if ok, _ := regexp.MatchString(`^[a-zA-Z0-9_]+$`, tenant); !ok {
+		return nil, fmt.Errorf("invalid tenant ID")
+	}
+
+	// Validación del nombre del schema
+	if !regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`).MatchString(tenant) {
+		return nil, fmt.Errorf("invalid tenant ID")
+	}
+
+	// Quoteo seguro
+	quotedSchema := QuoteIdentifier(tenant)
+	return &quotedSchema, nil
+
+}
