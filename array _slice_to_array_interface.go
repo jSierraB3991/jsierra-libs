f20a17c4216ace6a7d517c70a751c1a5042f27ee
Juan David Sierra <judas3991@gmail.com>
2025-06-24 12:18:57 -0500

    slice to array of interface

---
+++ array _slice_to_array_interface.go
@@ -0,0 +1,9 @@
+package eliotlibs
+
+func ToInterfaceSlice[T any](slice []T) []interface{} {
+	result := make([]interface{}, len(slice))
+	for i, v := range slice {
+		result[i] = v
+	}
+	return result
+}
