d8ec432d1b799d86c6869360b4d2db99b71e9723
Juan David Sierra <judas3991@gmail.com>
2025-07-02 10:54:46 -0500

    must message to invalid tenant id

--- multi-tenant.go
+++ multi-tenant.go
@@ -2,7 +2,7 @@ package eliotlibs
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"regexp"
 	"strings"
 )
@@ -26,16 +26,16 @@ func QuoteIdentifier(identifier string) string {
 func WithTenant(ctx context.Context) (*string, error) {
 	tenant, isOk := FromContext(ctx)
 	if !isOk {
-		return nil, fmt.Errorf("tenant ID not found in context")
+		return nil, errors.New("tenant ID not found in context")
 	}
 	// Validar de tenant ID
 	if ok, _ := regexp.MatchString(`^[a-zA-Z0-9_]+$`, tenant); !ok {
-		return nil, fmt.Errorf("invalid tenant ID")
+		return nil, errors.New("invalid characters in tenant")
 	}
 
 	// Validación del nombre del schema
 	if !regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`).MatchString(tenant) {
-		return nil, fmt.Errorf("invalid tenant ID")
+		return nil, errors.New("invalid characters in tenant")
 	}
 
 	// Quoteo seguro
