eeceeb02b7f69b5bff4574320910f81ba4a659d6
Juan David Sierra <judas3991@gmail.com>
2025-07-02 10:25:49 -0500

    add validation to find tenant

--- multitenant_middleware.go
+++ multitenant_middleware.go
@@ -6,17 +6,24 @@ import (
 	"regexp"
 )
 
-func GetContextWithTenantForMultenant(req *http.Request) context.Context {
-	tenantID := req.Header.Get("X-Tenant-ID")
-	if tenantID == "" {
-		tenantID = "public"
-	}
+func getContext(req *http.Request, tenantId string) context.Context {
 
-	matched, _ := regexp.MatchString(`^[a-zA-Z0-9_]+$`, tenantID)
+	matched, _ := regexp.MatchString(`^[a-zA-Z0-9_]+$`, tenantId)
 	if !matched {
 		return req.Context()
 	}
 
 	// Inyectar tenant en el contexto de la request
-	return context.WithValue(req.Context(), ContextTenantKey, tenantID)
+	return context.WithValue(req.Context(), ContextTenantKey, tenantId)
+}
+
+func GetContextWithTenantForMultenant(req *http.Request, schemasAllow []string) context.Context {
+	tenantID := req.Header.Get("X-Tenant-ID")
+
+	for _, v := range schemasAllow {
+		if v == tenantID {
+			return getContext(req, tenantID)
+		}
+	}
+	return nil
 }
