b177afc1bb4df9b3aa3dd436c2573fb4e54ce956
Juan David Sierra <judas3991@gmail.com>
2026-04-19 13:46:01 -0500

    fix add error to 'Method Not Allowed' and 'invalid format'

--- error_generic_errors.go
+++ error_generic_errors.go
@@ -6,3 +6,10 @@ type MethodNotAllowedError struct {
 func (e *MethodNotAllowedError) Error() string {
 	return "METHOD_NOT_ALLOWED"
 }
+
+type InvalidFormatError struct {
+}
+
+func (e *InvalidFormatError) Error() string {
+	return "INVALID_FORMAT"
+}
