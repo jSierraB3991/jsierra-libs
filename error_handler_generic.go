dbec2d30a8b396c94ba61a427cf859337f1d8bcc
Juan David Sierra <judas3991@gmail.com>
2026-04-20 16:48:59 -0500

    add generic configuraation to handler error

---
+++ error_handler_generic.go
@@ -0,0 +1,42 @@
+package eliotlibs
+
+import "net/http"
+
+func validateWithError(mesasge string, codReturn int, err error) *int {
+	if mesasge == err.Error() {
+		return &codReturn
+	}
+	return nil
+}
+
+func runMultipleValidationCode(message string, valueReturn int, errs ...error) *int {
+	for _, err := range errs {
+		code := validateWithError(message, valueReturn, err)
+		if code != nil {
+			return code
+		}
+	}
+	return nil
+}
+
+func GetErrorCodeMethodNotAllowed(message string) *int {
+	return runMultipleValidationCode(message, http.StatusMethodNotAllowed,
+		MethodNotAllowedError{},
+	)
+}
+
+func GetErrorCodeUnprocessableEntity(message string) *int {
+	return runMultipleValidationCode(message, http.StatusUnprocessableEntity,
+		InvalidFormatError{},
+	)
+}
+
+func RunValidationCodeSpecificErrors(message string, funcs ...func(string) *int) int {
+	for _, fn := range funcs {
+		code := fn(message)
+		if code != nil {
+			return *code
+		}
+	}
+	return http.StatusBadRequest
+}
